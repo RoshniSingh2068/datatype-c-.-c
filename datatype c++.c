2123c412dccdc3cf406b14123f095d6d75a5f83a
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:07:01 +0530

    Create datatype c++.c

---
+++ datatype c++.c
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    // creating my variable
+    int myNum = 5;
+    float myFloatNum = 5.99;
+    double myDoubleNum = 9.98;
+    char myLetter = 'D';
+    bool myBoolean = true;
+    string myString = "Hello";
+
+    // print variable value
+
+    cout << "int" << myNum << "\n";
+    cout << " float" << myFloatNum <<"\n";
+    cout << " double" <<  myDoubleNum  <<"\n";
+     cout << "char " <<  myLetter <<"\n";
+     cout << " bool" << myBoolean <<"\n";
+      cout << "string  " <<  myString <<"\n";
+     return 0;
+}
